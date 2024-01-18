358ac46fb32d0398f9aec507ea0de89aa2973c46
MadhumithaNagarajan-alt <152258960+MadhumithaNagarajan-alt@users.noreply.github.com>
2024-01-18 15:17:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+/*const clickMebtn= document.getElementById("click-me-id");
+clickMebtn.addEventListener("click", () => {
+    alert("hey");
+});*/
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+    document.getElementById("demo").style.color = "blue";
+    document.getElementById("demo").style.fontSize = "larger";
+}
